b5b3e3b57146e190f4db9070692a643d1ef28ede
Hanks <zhanghan.me@gmail.com>
2017-10-09 15:44:38 +0800

    * [jsfm] use  instead of  for compatibility

--- html5/runtime/vdom/Element.js
+++ html5/runtime/vdom/Element.js
@@ -408,7 +408,7 @@ export default class Element extends Node {
 
     if (!isStopPropagation
       && isBubble
-      && BUBBLE_EVENTS.includes(type)
+      && (BUBBLE_EVENTS.indexOf(type) !== -1)
       && this.parentNode
       && this.parentNode.fireEvent) {
       event.currentTarget = this.parentNode
