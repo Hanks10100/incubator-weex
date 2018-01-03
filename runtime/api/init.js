c3563afe51d8a68ab3f616a676951f6d7a1ac77b
Hanks <zhanghan.me@gmail.com>
2018-01-03 22:44:39 +0800

    * [jsfm] support sending class list

--- runtime/api/init.js
+++ runtime/api/init.js
@@ -108,6 +108,9 @@ function createInstanceContext (id, options = {}, data) {
   const runtimeContext = Object.create(null)
   Object.assign(runtimeContext, services, {
     weex,
+    __register_static_styles__ (...args) {
+      weex.document.registerStyleSheets(...args)
+    },
     services // Temporary compatible with some legacy APIs in Rax
   })
   Object.freeze(runtimeContext)
